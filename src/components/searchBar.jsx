85a118d5b7ff7cd30abcbc78d570c1fd9a273f27
Lorenzo Edwards <lorenzoedwards.0099@gmail.com>
2024-12-16 18:29:42 -0500

    easier

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -2,25 +2,17 @@ import React, { useState, useEffect } from 'react';
 // import pkg from 'react-router-dom';
 // import { useHistory } from "react-router-dom";
 
+import searchTerms from "./data/searchTerm.json";
+
 function SearchBar() {
 
-    const data = [
-        {searchTerm: "Formation Video : Slow Way", title: "Formation Video : Slow Way", link: "/formation/4"},
-        {searchTerm: "Formation Video : Rest", title: "Formation Video : Rest", link: "/formation/3"},
-        {searchTerm: "Formation Video : Child of the Creator", title: "Formation Video : Child of the Creator", link: "/formation/2"},
-        {searchTerm: "Formation Video : Colors of Fall", title: "Formation Video : Colors of Fall", link: "/formation/1"},
-        {searchTerm: " Vocatio Reimagined : Episode 1 : Creating Space in a Distracted World", title: "Episode 1 : Creating Space in a Distracted World", link: "/podcasts/1"},
-        {searchTerm: " Vocatio Reimagined : Episode 1 : Radical Hospitality", title: "Episode 2 : Radical Hospitality", link: "/podcasts/2"},
-        {searchTerm: " Vocatio Reimagined : Episode 1 : Creating Space in a Distracted World", title: "Episode 3 : Creating Space in a Distracted World", link: "/podcasts/3"},
-        {searchTerm: " Vocatio Reimagined : Episode 1 : When Your Calling and Career Don't Align", title: "Episode 4 : When Your Calling and Career Don't Align", link: "/podcasts/4"}
-    ]
   const [searchTerm, setSearchTerm] = useState('');
   const [suggestions, setSuggestions] = useState([]);
 //   const navigate = useHistory();
 
   useEffect(() => {
     if (searchTerm.length > 0) {
-      const filteredSuggestions = data.filter(item =>
+      const filteredSuggestions = searchTerms.filter(item =>
         item.searchTerm.toLowerCase().includes(searchTerm.toLowerCase())
       );
       setSuggestions(filteredSuggestions.slice(0, 10)); // Limit to 10 suggestions
